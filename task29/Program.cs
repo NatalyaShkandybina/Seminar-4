1a5e12b9209c5d36fcc7831ed4ac24fda89cddab
Natalya Shkandybina <natalya.shkandybina@mail.ru>
2022-08-24 12:44:40 +0300

    info about task29

---
+++ task29/Program.cs
@@ -0,0 +1,18 @@
+﻿// (Вводим с помощью клавиатуры, т.е. через консоль)
+Console.Write ("Введите число элементов массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [length];
+for (int i = 0; i < array.Length; i++)
+    {
+      Console.Write ("Введите элемент массива под индексом "+i+": ");
+      array [i] = Convert.ToInt32 (Console.ReadLine ());
+    }
+
+for (int i = 0; i < array.Length; i++)
+    {
+          Console.Write (array[i]);
+          if (i!=array.Length - 1)
+            {
+              Console.Write (", ");
+            }
+    }
